d6c022797d5b83e291856c33a0c987d58f6842da
yacobls <yacob.lamrodo@gmail.com>
2024-11-26 14:54:28 +0700

    Update bug in Login.js

---
+++ frontend/src/screens/Login.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Message from "../components/LoadingError/Error";
+import Loading from "../components/LoadingError/Loading";
+import Header from "./../components/Header";
+import { login } from "./../Redux/Actions/userActions";
+import { withRouter } from "react-router-dom";  // Import withRouter
+
+const Login = ({ location, history }) => {  // Pastikan history dan location ada di props
+  window.scrollTo(0, 0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { error, loading, userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);  // Menggunakan history.push() untuk berpindah halaman
+    }
+  }, [userInfo, history, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container d-flex flex-column justify-content-center align-items-center login-center">
+        {error && <Message variant="alert-danger">{error}</Message>}
+        {loading && <Loading />}
+        <form
+          className="Login col-md-8 col-lg-4 col-11"
+          onSubmit={submitHandler}
+        >
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          <p>
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : "/register"}
+            >
+              Create Account
+            </Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default withRouter(Login);  // Membungkus Login dengan withRouter
