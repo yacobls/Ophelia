9b7dc4898e0f566cb5ba0620710bdc365b92c87c
yacobls <yacob.lamrodo@gmail.com>
2024-11-26 19:38:31 +0700

    Add API & Redux On Payment

--- frontend/src/Redux/Actions/OrderActions.js
+++ frontend/src/Redux/Actions/OrderActions.js
@@ -4,11 +4,14 @@ import {
   ORDER_CREATE_SUCCESS,
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
 } from "../Constants/OrderConstants";
 import axios from "axios";
 import { CART_CLEAR_ITEMS } from "../Constants/CartConstants";
 import { logout } from "./userActions";
-import { ORDER_DETAILS_REQUEST } from './../Constants/OrderConstants';
+import { ORDER_DETAILS_REQUEST } from "./../Constants/OrderConstants";
 
 // CREATE ORDER
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -77,3 +80,41 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+// ORDER PAY
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({ type: ORDER_PAY_REQUEST });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+      dispatch({ type: ORDER_PAY_SUCCESS, payload: data });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      if (message === "Not authorized, token failed") {
+        dispatch(logout());
+      }
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload: message,
+      });
+    }
+  };
