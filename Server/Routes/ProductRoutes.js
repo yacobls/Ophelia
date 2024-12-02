f9791116f5b4f1126b90269a84f9d580286a0a8f
yacobls <yacob.lamrodo@gmail.com>
2024-12-03 04:36:48 +0700

    Add Create Product (API)

--- Server/Routes/ProductRoutes.js
+++ Server/Routes/ProductRoutes.js
@@ -108,4 +108,35 @@ productRoute.delete(
   })
 );
 
+// CREATE PRODUCT
+productRoute.post(
+  "/",
+  protect,
+  admin,
+  asyncHandler(async (req, res) => {
+    const { name, price, description, image, countInStock } = req.body;
+    const productExist = await Product.findOne({ name });
+    if (productExist) {
+      res.status(400);
+      throw new Error("Product name already exist");
+    } else {
+      const product = new Product({
+        name,
+        price,
+        description,
+        image,
+        countInStock,
+        user: req.user._id,
+      });
+      if (product) {
+        const createdproduct = await product.save();
+        res.status(201).json(createdproduct);
+      } else {
+        res.status(400);
+        throw new Error("Invalid product data");
+      }
+    }
+  })
+);
+
 export default productRoute;
